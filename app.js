da1f4673156724480d0fcf3d49853b14499a6916
prajapathi deepak <94177698+ddeeppak@users.noreply.github.com>
2024-11-04 15:21:35 +0900

    gcp

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-var Application = require("./lib/app");
-var Server      = require("./lib/server");
-var sdk         = require("./lib/sdk");
-var config      = require("./config");
+const Application = require("./lib/app");
+const Server = require("./lib/server");
+const sdk = require("./lib/sdk");
+const config = require("./config");
 
-var app    = new Application(null, config);
-var server = new Server(config, app);
+const app = new Application(null, config);
+const server = new Server(config, app);
 
 sdk.checkNodeVersion();
 
-server.start();
-
-// sdk.registerBot(require('./FindAFlight.js'));
+// Register only the bots you need
 sdk.registerBot(require('./SimpleConversationalBot.js'));
-// sdk.registerBot(require('./SimpleConversationalBotWithMultipleBotId.js'));
-// sdk.registerBot(require('./GuessTheNumber.js'));
-// sdk.registerBot(require('./BookACab.js'));
-// sdk.registerBot(require('./OrderAPizza.js'));
-// sdk.registerBot(require('./BotVariables.js'));
-// sdk.registerBot(require('./LiveChat.js'));
+
+const PORT = process.env.PORT || 8080;
+server.start(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
